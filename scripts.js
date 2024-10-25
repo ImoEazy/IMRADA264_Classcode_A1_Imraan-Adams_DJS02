1ad85238735f9d7782df48e389f0ef6ced2fa73d
ImoEazy <imieadams@gmail.com>
2024-10-25 23:16:21 +0200

    add const and parsnet function

--- scripts.js
+++ scripts.js
@@ -5,5 +5,12 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
+  const dividendNum = parseInt(dividend, 10);
+  const dividerNum = parseInt(divider, 10);
+
+  if (isNaN(dividendNum) || isNaN(dividerNum)) { 
   result.innerText = dividend / divider;
+  return;
 });
+
+
