bb7d6043251cf57adf48ca60b34c6931fa34f711
ImoEazy <imieadams@gmail.com>
2024-10-29 13:16:15 +0200

    addded notes

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-
+  //check if either dividende or divider input is empty
   if (dividend === "" || divider === "") {
     result.innerText = `divisiion not incorrect, both inputs need values.`;
     return;
@@ -17,19 +17,20 @@ form.addEventListener("submit", (event) => {
   // checks for special characters, letters from input
   const regEX = /^[a-zA-Z]+$/;
 
-
+  //if divide by zero throw error
   try {
     if (dividerNumber === 0){
       throw new Error ("division cant be performed, Invalid number.");
     }
+    //if either input contains letters/characters throw errror. Input validation
     if (regEX.test(dividend) || regEX.test(divider)) {
       throw new Error("Something critical went wrong. Please reload the page.");
     }
-
+    //if no errors then perform calculation. use math.floor to round down for whole numbbers
     result.innerText = Math.floor(dividendNumber/dividerNumber);
     } catch (error) {
     console.error(Error);
-
+    //If an error occurs during the division,(undefined or NaN), the code enters the catch block. It logs the error to the console for debugging purposes.
     if (error.message.includes('Invalid number')) {
       result.innerText = error.message;
     }
